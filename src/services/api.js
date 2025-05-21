a9869849592e48b1ad55bef97beea32dbc1a2110
dmovss <d.vovk2018@gmail.com>
2025-05-21 23:04:28 +0100

    1
    229

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,9 @@
-// src/services/api.js
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_UNSPLASH_API_KEY;
-
-axios.defaults.baseURL = 'https://api.unsplash.com';
-
-export const fetchImages = async (query, page = 1, perPage = 12) => {
-  try {
-    const response = await axios.get('/search/photos', {
-      params: {
-        query,
-        page,
-        per_page: perPage,
-        client_id: API_KEY,
-      },
-    });
-    return response.data.results;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    throw error;
-  }
+export const fetchImages = async (query, page, signal) => {
+  const response = await axios.get(
+    `https://api.unsplash.com/search/photos?client_id=2GFk4d0ZaotLby7WLy2tWmmk4g-7OLck-BWzc2I4VJs&query=${query}&page=${page}`,
+    { signal }
+  );
+  return response.data.results;
 };
